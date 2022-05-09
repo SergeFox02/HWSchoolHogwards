a263338810f4b1e94d5cdf9ce1b56e72ac013375
SergeFox02 <Fox2188@gmail.com>
2022-05-09 17:37:00 +0300

    hw 4.6 Threads

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -35,4 +35,8 @@ public interface StudentService {
     OptionalDouble averageAge();
 
     int findValue();
+
+    void printAllStudentsToConsole();
+
+    void printAllStudentsToConsoleSynchronized();
 }
