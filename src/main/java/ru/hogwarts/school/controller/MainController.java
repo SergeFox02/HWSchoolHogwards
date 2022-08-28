b92fe7545362577a8b199e58d4b0e1b52bc60528
SergeFox02 <Fox2188@gmail.com>
2022-08-28 13:23:10 +0300

    Update MainController

--- src/main/java/ru/hogwarts/school/controller/MainController.java
+++ src/main/java/ru/hogwarts/school/controller/MainController.java
@@ -1,13 +1,35 @@
 package ru.hogwarts.school.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @Operation(
+            summary = "Welcome message in School Hogwards",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Send a welcome message",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE,
+                                    examples = @ExampleObject(value = "string"))
+                    )
+            },
+            tags = "School Hogwards (home page)"
+    )
     @GetMapping
-    public String helloHogwards() {
+    public String helloMessage() {
+        logger.info("Call helloMessage in MainController");
         return "Hello yang Mag, in our School";
     }
 
