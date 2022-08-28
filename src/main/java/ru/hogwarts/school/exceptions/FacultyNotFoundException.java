45db24aed81bedf878b6c04d7aa9004e8dac15e5
SergeFox02 <Fox2188@gmail.com>
2022-08-28 17:16:08 +0300

    Delete package exceptions

--- src/main/java/ru/hogwarts/school/exceptions/FacultyNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.hogwarts.school.exceptions;
-
-public class FacultyNotFoundException extends RuntimeException{
-    public FacultyNotFoundException(String message) {
-        super(message);
-    }
-}
