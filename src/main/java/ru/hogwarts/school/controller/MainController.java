1f02c3cfc2cc5f518f767711ca3b6649bfb7e9e1
SergeFox02 <Fox2188@gmail.com>
2022-08-28 13:11:45 +0300

    Small refactor code

--- src/main/java/ru/hogwarts/school/controller/MainController.java
+++ src/main/java/ru/hogwarts/school/controller/MainController.java
@@ -2,24 +2,13 @@ package ru.hogwarts.school.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.hogwarts.school.service.StudentService;
 
 @RestController
 public class MainController {
-
-    private final StudentService studentService;
-
-    public MainController(StudentService studentService) {
-        this.studentService = studentService;
-    }
 
     @GetMapping
     public String helloHogwards() {
         return "Hello yang Mag, in our School";
     }
 
-    @GetMapping("/find-value")
-    public int findValue() {
-        return studentService.findValue();
-    }
 }
