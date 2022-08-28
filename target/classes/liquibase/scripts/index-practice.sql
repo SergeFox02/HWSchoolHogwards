18376ece267d4b6790c1276baf237e9470dbbbf0
Serge Philippov <94442157+SergeFox02@users.noreply.github.com>
2022-08-28 13:27:46 +0300

    Delete target directory

--- target/classes/liquibase/scripts/index-practice.sql
+++
@@ -1,7 +0,0 @@
--- liquibase formatted sql
-
--- changeSet serge:1
-CREATE INDEX student_name_index ON student (name);
-
--- changeSet serge:2
-CREATE INDEX faculty_name_and_color_index ON faculty (name, color);
